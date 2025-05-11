501f06708d9e19841c0b541356b1e7bdc210dc48
Arsh Gupta <apple@apples-MacBook-Pro.local>
2025-05-11 19:41:12 +0530

    VivekLovesArrayGame done

--- src/recursionAssignment/VivekLovesArrayGame.java
+++ src/recursionAssignment/VivekLovesArrayGame.java
@@ -13,41 +13,37 @@ public class VivekLovesArrayGame {
 			for (int i = 0; i < n; i++) {
 				arr[i] = sc.nextInt();
 			}
-//			System.out.println(divideEqualArray(arr, 0, arr.length - 1));
-			for (int i = 0; i < arr.length; i++) {
-				System.out.println(divideEqualArray(arr, i));
-			}
-//			 int points = arrayGame(arr, 0, arr.length - 1);
-//			System.out.println(points);
+			System.out.println(arrayGame(arr, 0, arr.length - 1));
 			t--;
 		}
 		sc.close();
 	}
 
-	public static void arrayGame(int[] arr, int begIdx, int endIdx) {
-		
-		for (int i = begIdx; i < endIdx; i++) {
-			int idx = divideEqualArray(arr, begIdx);
-			if (idx != -1) {
-				arrayGame(arr, begIdx, idx);
-				arrayGame(arr, idx + 1, endIdx);
+	public static int arrayGame(int[] arr, int begIdx, int endIdx) {
+		if (begIdx >= endIdx) {
+			return 0;
+		}
+		for (int i = begIdx; i <= endIdx; i++) {
+			if (divide(arr, begIdx, endIdx, i) != -1) {
+				return 1 + Math.max(arrayGame(arr, begIdx, i), arrayGame(arr, i + 1, endIdx));
 			}
 		}
+		return 0;
 	}
 
-	public static int divideEqualArray(int[] arr, int idx) {
-		int sum1 = 0;
-		for (int i = 0; i <= idx; i++) {
-			sum1 += arr[i];
+	public static int divide(int[] arr, int begIdx, int endIdx, int idx) {
+		int sumLeft = 0;
+		for (int i = begIdx; i <= idx; i++) {
+			sumLeft += arr[i];
 		}
-		int sum2 = 0;
-		for (int i = idx + 1; i < arr.length; i++) {
-			sum2 += arr[i];
+
+		int sumRight = 0;
+		for (int i = idx + 1; i <= endIdx; i++) {
+			sumRight += arr[i];
 		}
-		if (sum1 == sum2) {
+
+		if (sumLeft == sumRight)
 			return idx;
-		} else {
-			return -1;
-		}
+		return -1;
 	}
 }
