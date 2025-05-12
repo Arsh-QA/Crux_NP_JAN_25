a2f9e1a7059a77910a5f5c72004ea9abbe18f0d0
Arsh Gupta <apple@apples-MacBook-Pro.local>
2025-05-12 22:53:47 +0530

    split array

--- src/recursionAssignment/SplitArrayRecursion.java
+++ src/recursionAssignment/SplitArrayRecursion.java
@@ -11,48 +11,37 @@ public class SplitArrayRecursion {
 		for (int i = 0; i < n; i++) {
 			arr[i] = sc.nextInt();
 		}
-//		splitArray(arr, 0);
-		recursionArray(arr, 0, 0);
+		recursionArray(arr, 0, n - 1);
 		sc.close();
 	}
 
-	public static void recursionArray(int[] arr, int idx, int sum) {
-		System.out.println("Entered Recursion function");
-		if (idx == arr.length)
-			return;
-		sum += arr[idx];
-		if (sum == sumOfRest(arr, idx + 1)) {
-			for (int j = 0; j < arr.length; j++) {
-				if (j == idx)
-					System.out.print(arr[j] + " and ");
-				else
-					System.out.print(arr[j] + " ");
-			}
-		}
-		recursionArray(arr, idx + 1, sum);
-		sum -= arr[idx];
+	public static void recursionArray(int[] arr, int si, int ei) {
+
 	}
 
-	public static void splitArray(int[] arr, int idx) {
-		int sum = 0;
-		for (int i = 0; i < arr.length; i++) {
-			sum += arr[i];
-			if (sum == sumOfRest(arr, i)) {
-				for (int j = 0; j < arr.length; j++) {
-					if (j == i)
-						System.out.print(arr[j] + " and ");
-					else
-						System.out.print(arr[j] + " ");
-				}
-			}
+	public static void display(int[] arr, int i) {
+		for (int idx = 0; idx < arr.length; idx++) {
+			if (idx == i)
+				System.out.print(arr[idx] + " and ");
+			else
+				System.out.print(arr[idx] + " ");
 		}
 	}
 
-	public static int sumOfRest(int[] arr, int i) {
-		int sum = 0;
-		for (int j = i; j < arr.length; j++) {
-			sum += arr[j];
+	public static int divide(int[] arr, int begIdx, int endIdx, int idx) {
+		int sumLeft = 0;
+		for (int i = begIdx; i <= idx; i++) {
+			sumLeft += arr[i];
 		}
-		return sum;
+
+		int sumRight = 0;
+		for (int i = idx + 1; i <= endIdx; i++) {
+			sumRight += arr[i];
+		}
+
+		if (sumLeft == sumRight)
+			return idx;
+		return -1;
 	}
+
 }
