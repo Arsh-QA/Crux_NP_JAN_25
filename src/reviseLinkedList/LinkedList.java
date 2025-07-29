f9edab22176fca4e1195b2964e71dc47ba42a61a
Arsh Gupta <apple@apples-MacBook-Pro.local>
2025-07-29 23:58:12 +0530

    revise LinkedList

---
+++ src/reviseLinkedList/LinkedList.java
@@ -0,0 +1,41 @@
+package reviseLinkedList;
+
+public class LinkedList {
+	class Node {
+		int data;
+		Node next;
+	}
+
+	private Node head;
+	private Node tail;
+	private int size;
+
+	// O(1)
+	public void addFirst(int item) {
+		Node nn = new Node();
+		nn.data = item;
+		if (size == 0) {
+			head = nn;
+			tail = nn;
+			size++;
+		} else {
+			nn.next = head;
+			head = nn;
+			size++;
+		}
+	}
+
+	public void display() {
+		Node temp = head;
+		while (temp != null) {
+			System.out.print(temp.data + "-->");
+			temp = temp.next;
+		}
+		System.out.print(".");
+		System.out.println();
+	}
+
+	public void size() {
+		System.out.println(size);
+	}
+}
---
+++ src/reviseLinkedList/LinkedListClient.java
@@ -0,0 +1,13 @@
+package reviseLinkedList;
+
+public class LinkedListClient {
+
+	public static void main(String[] args) {
+		LinkedList ll = new LinkedList();
+		ll.addFirst(10);
+		ll.addFirst(20);
+		ll.addFirst(30);
+		ll.display();
+		ll.size();
+	}
+}
