e72b775999399c6a636e8d86816a9cab200794f7
Arsh Gupta <apple@apples-MacBook-Pro.local>
2025-07-30 23:32:58 +0530

    revise Linked List2

--- src/reviseLinkedList/LinkedList.java
+++ src/reviseLinkedList/LinkedList.java
@@ -17,14 +17,105 @@ public class LinkedList {
 		if (size == 0) {
 			head = nn;
 			tail = nn;
-			size++;
 		} else {
 			nn.next = head;
 			head = nn;
+		}
+		size++;
+	}
+
+	// O(1)
+	public void addLast(int item) {
+		if (size == 0) {
+			addFirst(item);
+		} else {
+			Node nn = new Node();
+			nn.data = item;
+			tail.next = nn;
+			tail = nn;
 			size++;
 		}
 	}
 
+	// O(N)
+	public void addAtIndex(int item, int idx) throws Exception {
+		if (idx == 0) {
+			addFirst(item);
+		} else if (idx == size) {
+			addLast(item);
+		} else {
+			Node nn = new Node();
+			nn.data = item;
+			Node idx_1th = getNode(idx - 1);
+			nn.next = idx_1th.next;
+			idx_1th.next = nn;
+			size++;
+		}
+	}
+
+	// O(1)
+	public int getFirst() {
+		return head.data;
+	}
+
+	// O(1)
+	public int getLast() {
+		return tail.data;
+	}
+
+	// O(N)
+	public int getAtIndex(int idx) throws Exception {
+		Node nn = getNode(idx);
+		return nn.data;
+	}
+
+	// O(1)
+	public int removeFirst() {
+		int rv = head.data;
+		if (size == 1) {
+			head = null;
+			tail = null;
+		} else {
+			Node temp = head;
+			head = head.next;
+			temp.next = null;
+		}
+		size--;
+		return rv;
+	}
+
+	// O(N)
+	public int removeLast() throws Exception {
+		if (size == 1)
+			return removeFirst();
+		else {
+			int rv = tail.data;
+			Node sl = getNode(size - 2);
+			sl.next = null;
+			tail = sl;
+			size--;
+			return rv;
+		}
+	}
+
+	// O(N)
+	public int removeAtIndex(int idx) throws Exception {
+		if (idx == 0)
+			return removeFirst();
+		else if (idx == size - 1)
+			return removeLast();
+		else {
+			Node prev = getNode(idx - 1);
+			Node nn = prev.next;
+			int rv = nn.data;
+			prev.next = nn.next;
+			nn.next = null;
+			size--;
+			return rv;
+		}
+	}
+
+	// O(N)
 	public void display() {
 		Node temp = head;
 		while (temp != null) {
@@ -35,7 +126,17 @@ public class LinkedList {
 		System.out.println();
 	}
 
-	public void size() {
-		System.out.println(size);
+	private Node getNode(int idx) throws Exception {
+		if (idx < 0 || idx > size)
+			throw new Exception("Index out of range");
+		Node temp = head;
+		for (int i = 0; i < idx; i++) {
+			temp = temp.next;
+		}
+		return temp;
+	}
+
+	public int size() {
+		return size;
 	}
 }
--- src/reviseLinkedList/LinkedListClient.java
+++ src/reviseLinkedList/LinkedListClient.java
@@ -2,12 +2,26 @@ package reviseLinkedList;
 
 public class LinkedListClient {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		LinkedList ll = new LinkedList();
 		ll.addFirst(10);
 		ll.addFirst(20);
 		ll.addFirst(30);
+		ll.addLast(40);
+		ll.addAtIndex(-20, 2);
+		ll.addLast(42);
+		ll.addLast(67);
+		ll.addLast(90);
 		ll.display();
-		ll.size();
+		System.out.println(ll.size());
+//		System.out.println(ll.getFirst());
+//		System.out.println(ll.getLast());
+//		System.out.println(ll.getAtIndex(2));
+		System.out.println(ll.removeFirst());
+		ll.display();
+		System.out.println(ll.removeLast());
+		ll.display();
+		System.out.println(ll.removeAtIndex(2));
+		ll.display();
 	}
 }
